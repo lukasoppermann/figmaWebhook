f9222e50e4d6e1cf32583f9adf341df5d4b8cad5
Lukas Oppermann <lukasoppermann@github.com>
2022-12-15 14:22:55 +0100

    add mustache template

---
+++ .github/actions/create-release-note.ts
@@ -0,0 +1,9 @@
+// import Mustache from 'mustache';
+
+// function renderReleaseNotes() {
+//   fetch('template.mustache')
+//     .then((response) => response.text())
+//     .then((template) => Mustache.render(template, { name: 'Luke' }););
+// }
+
+// var output = Mustache.render("{{title}} spends {{calc}}", view);
