db8d95eba76f5bcf9602bbabf273989cd6260881
Lukas Oppermann <lukasoppermann@github.com>
2022-12-16 13:25:27 +0100

    working templaet

--- .github/actions/create-changeset.ts
+++ .github/actions/create-changeset.ts
@@ -2,18 +2,13 @@
 import Mustache from 'mustache';
 import { readFileSync, writeFileSync } from 'fs';
 
-const renderWithTemplate = async (template: string, data: Record<string, unknown>): Promise<string> => {
-  return fetch(template)
-    .then((response) => response.text())
-    .then((template) => Mustache.render(template, data))
+const renderWithTemplate = (template: string, data: Record<string, unknown>): Promise<string> => {
+  const templateContent = readFileSync(template).toString()
+  return Mustache.render(templateContent, data)
 }
 
 const json = readFileSync('./temp-api-response.json')
 const parsedJson = JSON.parse(json.toString())
 
-const writeChangeset = async () => {
-  const changeset = await renderWithTemplate('../templates/default-release-note.mustache', parsedJson)
-  writeFileSync(`.changeset/${parsedJson.fileInfo.fileName}-${parsedJson.fileInfo.timestamp}.md`, changeset)
-}
-
-writeChangeset()
+const changeset = renderWithTemplate('.github/templates/default-release-note.mustache', parsedJson)
+writeFileSync(`.changeset/${parsedJson.fileInfo.fileName}-${parsedJson.fileInfo.timestamp}.md`, changeset)
