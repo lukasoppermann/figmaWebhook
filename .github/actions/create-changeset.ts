b3b92bc796d4adf721fcbdcfba2df3359ea5eb5b
Lukas Oppermann <lukasoppermann@github.com>
2022-12-20 17:11:12 +0100

    replace mustache templaet with es6

--- .github/actions/create-changeset.ts
+++ .github/actions/create-changeset.ts
@@ -1,14 +1,15 @@
 // import json from './temp-api-response.json'
-import Mustache from 'mustache';
+// import Mustache from 'mustache';
+import { render } from '../templates/default-release-note'
 import { readFileSync, writeFileSync } from 'fs';
 
-const renderWithTemplate = (template: string, data: Record<string, unknown>): Promise<string> => {
-  const templateContent = readFileSync(template).toString()
-  return Mustache.render(templateContent, data)
-}
+// const renderWithTemplate = (template: string, data: Record<string, unknown>): Promise<string> => {
+//   const templateContent = readFileSync(template).toString()
+//   return Mustache.render(templateContent, data)
+// }
 
 const json = readFileSync('./temp-api-response.json')
 const parsedJson = JSON.parse(json.toString())
 
-const changeset = renderWithTemplate('.github/templates/default-release-note.mustache', parsedJson)
+const changeset = render(parsedJson).replace(/\n{2,}/g, "\n")
 writeFileSync(`.changeset/${parsedJson.fileInfo.fileName}-${parsedJson.fileInfo.timestamp}.md`, changeset)
